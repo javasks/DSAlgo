789c959fc3cde2383b063f56ec5c4064e8ceaedc
sandeep <ankurmall89@gmail.com>
2020-02-27 23:27:33 +0530

    Added Reverse String from hacker rank

---
+++ src/com/sks/hkr/ReverseString.java
@@ -0,0 +1,28 @@
+package com.sks.hkr;
+
+import java.util.*;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        char ch[]=A.toCharArray();
+        String rev="";
+        for(int i=ch.length-1;i>=0;i--){
+            rev+=ch[i];
+        }
+        if(A.equals(rev)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+
+    }
+}
+
+
+
+
