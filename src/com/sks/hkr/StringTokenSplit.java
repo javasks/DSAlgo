0081eb1317fb6d6fc1b73732570d2d50c61b9451
sandeep <ankurmall89@gmail.com>
2020-03-14 09:11:48 +0530

    Added String Token

---
+++ src/com/sks/hkr/StringTokenSplit.java
@@ -0,0 +1,24 @@
+package com.sks.hkr;
+
+import java.util.*;
+
+public class StringTokenSplit {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        //String[] terms = s.split("[^a-zA-Z]+");
+        // Arrays.stream(terms).forEach(System.out::println);
+        String[] a = s.trim().split("[ !,?\\._'@]+", 0);
+        if (a.length == 1 && a[0].equals("")) {
+            System.out.println(0);
+        } else {
+            System.out.println(a.length);
+            Arrays.stream(a).forEach(System.out::println);
+        }
+        scan.close();
+    }
+
+
+}
