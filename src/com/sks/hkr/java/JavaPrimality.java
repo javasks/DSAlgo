9a45ccd15c23b0a171a9c94b99eadf204e1bbccf
sandeep <ankurmall89@gmail.com>
2020-03-15 12:28:43 +0530

    Prime and Pattern

---
+++ src/com/sks/hkr/java/JavaPrimality.java
@@ -0,0 +1,39 @@
+package com.sks.hkr.java;
+
+import java.util.Scanner;
+
+
+public class JavaPrimality {
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        /**
+         * Other approach
+
+         BigInteger bigInt=scanner.nextBigInteger();
+         System.out.println(bigInt.isProbablePrime(100) ? "prime" : "not prime");
+         scanner.close();
+         */
+
+
+        String num = scanner.nextLine();
+        int n = Integer.parseInt(num);
+        boolean flag = false;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+            System.out.println("prime");
+        else
+            System.out.println(" is not a prime number.");
+        scanner.close();
+    }
+
+}
+
